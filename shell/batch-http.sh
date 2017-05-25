14070dc4a6a169f6fed78d3c185e2b6fab636f65
zhangpan66 <zhangpan6@jd.com>
2017-05-25 19:46:42 +0800

    curl批量发http请求

--- shell/batch-http.sh
+++ shell/batch-http.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 #批量发起重新下单请求
+#导出浏览器cookie工具https://chrome.google.com/webstore/detail/cookiestxt/njabckikapfpffapmjgojcnbfjonfjfg
+#orderlist.txt文件是要重发的订单ID
 for line in `cat /Users/pan/Desktop/orderlist.txt`
 do
     curl --request GET \
       --cookie /Users/pan/Desktop/cookies.txt \
-      --url http://man.jyk.jd.com/order/againRecharge/$line \
+      --url http://****.jd.com/****againRecharge/$line \
       --header 'cache-control: no-cache'
 done
